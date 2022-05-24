dd98b98d4c1298a66357a84ab9201f1218b81245
unknown <nandhinimadan20@gmail.com>
2022-05-24 16:16:36 -0700

    installed cookie-parser

--- simplelearn/src/api/server/server.js
+++ simplelearn/src/api/server/server.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const helmet=require('helmet');
+const cors= require('cors')
 const server=express();
 server.use(helmet());
+server.use(cors())
 server.get('/',(req,res)=>{
     res.status(200).json({
         message:"Server running success"
